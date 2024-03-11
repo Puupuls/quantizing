0f5fcf2501ceabdbf0de903ccb8848cbb0d1abbc
Paul <paulspurvins@gmail.com>
2024-03-11 14:20:31 +0200

    Cleanup

--- quantize_awq.py
+++ quantize_awq.py
@@ -6,12 +6,12 @@ model_name = 'mistralai/Mistral-7B-v0.1'
 quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
 
 # Load model
-model = AutoAWQForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+model = AutoAWQForCausalLM.from_pretrained(model_name, cache_dir="cache/")
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, cache_dir="cache/")
 
 # Quantize
 model.quantize(tokenizer, quant_config=quant_config)
 
 # Save quantized model
-model.save_quantized(f'{model_name}/awq/')
-tokenizer.save_pretrained(f'{model_name}/awq/')
+model.save_quantized(f'outputs/{model_name}/awq/')
+tokenizer.save_pretrained(f'outputs/{model_name}/awq/')
